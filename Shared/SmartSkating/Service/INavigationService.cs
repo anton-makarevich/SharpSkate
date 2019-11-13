03f72535cd8067df2ab4e4fca4ade98f6eb4301e
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-11-13 23:38:13 +0100

    Add base MVVM files

---
+++ Shared/SmartSkating/Service/INavigationService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Sanet.SmartSkating.ViewModels.Base;
+
+namespace Sanet.SmartSkating.Service
+{
+    public interface INavigationService
+    {
+        T GetNewViewModel<T>() where T : BaseViewModel;
+        T GetViewModel<T>() where T : BaseViewModel;
+        bool HasViewModel<T>() where T : BaseViewModel;
+
+        Task NavigateToViewModelAsync<T>(T viewModel) where T : BaseViewModel;
+        Task NavigateToViewModelAsync<T>() where T : BaseViewModel;
+
+        Task ShowViewModelAsync<T>(T viewModel) where T : BaseViewModel;
+        Task ShowViewModelAsync<T>() where T : BaseViewModel;
+        
+        Task<TResult> ShowViewModelForResultAsync<T, TResult>(T viewModel) where T : BaseViewModel where TResult : class;
+        Task<TResult> ShowViewModelForResultAsync<T, TResult>() where T : BaseViewModel where TResult : class;
+
+        Task NavigateBackAsync();
+        Task CloseAsync();
+        Task NavigateToRootAsync();
+    }
+}
