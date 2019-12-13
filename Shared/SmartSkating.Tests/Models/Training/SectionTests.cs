f2fe54475fc6f13af088a0a66ab8f208442d97a2
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-12-13 23:13:03 +0100

    Refactor section tests

--- Shared/SmartSkating.Tests/Models/Training/SectionTests.cs
+++ Shared/SmartSkating.Tests/Models/Training/SectionTests.cs
@@ -7,38 +7,39 @@ namespace Sanet.SmartSkating.Tests.Models.Training
 {
     public class SectionTests
     {
-        private readonly DateTime _startTime;
-        private readonly DateTime _finishTime;
-        private readonly Coordinate _location;
+        private readonly Section _sut;
+
+        private const WayPointTypes StatType = WayPointTypes.Start;
+        private const WayPointTypes FinishType = WayPointTypes.Finish;
 
         public SectionTests()
         {
-            _startTime = new DateTime(2019,12,13,11,10,9);
-            _finishTime = new DateTime(2019,12,13,11,10,19);
-            _location = new Coordinate();
+            var startTime = new DateTime(2019,12,13,11,10,9);
+            var finishTime = new DateTime(2019,12,13,11,10,19);
+            var location = new Coordinate();
+            var startWayPoint = new WayPoint(
+                location,
+                location,
+                startTime,
+                StatType);
+            var finishWayPoint = new WayPoint(
+                location,
+                location,
+                finishTime,
+                FinishType);
+            _sut = new Section(startWayPoint,finishWayPoint);
         }
 
         [Fact]
         public void SectionFromStartToFinishIsFirstSector()
         {
-            const WayPointTypes statType = WayPointTypes.Start;
-            const WayPointTypes finishType = WayPointTypes.Finish;
-            
-            var startWayPoint = new WayPoint(
-                _location,
-                _location,
-                _startTime,
-                statType);
-            
-            var finishWayPoint = new WayPoint(
-                _location,
-                _location,
-                _finishTime,
-                finishType);
-            
-            var sut = new Section(startWayPoint,finishWayPoint);
-            
-            Assert.Equal(WayPointTypes.FirstSector, sut.Type);
+            Assert.Equal(WayPointTypes.FirstSector, _sut.Type);
+        }
+
+        [Fact]
+        public void SectionHasTimeFromStartToFinish()
+        {
+            Assert.Equal(10,_sut.Time.Seconds);
         }
     }
 }
