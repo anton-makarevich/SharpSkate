f9e0640da34fb1c1f706392cc59786ce7c8d2fbe
Anton Makarevich <amakarevich@mobiquityinc.com>
2019-12-21 00:26:21 +0100

    Reference tizen to shared project instead of mobile xf project

---
+++ Client/Watch/SmartSkating.Tizen/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Sanet.SmartSkating.ViewModels;
+using Sanet.SmartSkating.Xf.Services;
+using Sanet.SmartSkating.Xf.Views;
+using SimpleInjector;
+using Xamarin.Forms;
+
+namespace Sanet.SmartSkating.Xf
+{
+    public partial class App
+    {
+        public App(Container container)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new TracksView()
+            {
+                ViewModel = new XamarinFormsNavigationService(container).GetViewModel<TracksViewModel>()
+            });
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
