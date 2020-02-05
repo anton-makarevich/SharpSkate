f1c98bc0270340675c01a2ec2ee8de4346fab490
Anton Makarevich <amakarevich@mobiquityinc.com>
2020-02-06 00:00:37 +0100

    Update packages and linker

--- Client/Dashboard/SmartSkating.Dashboard.Wasm/Program.cs
+++ Client/Dashboard/SmartSkating.Dashboard.Wasm/Program.cs
@@ -1,12 +1,56 @@
-﻿namespace Sanet.SmartSkating.Dashboard.Wasm
+﻿using Microsoft.Extensions.Logging;
+
+namespace Sanet.SmartSkating.Dashboard.Wasm
 {
 	public class Program
 	{
 		static int Main(string[] args)
 		{
+			ConfigureFilters(Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory);
             Windows.UI.Xaml.Application.Start(_ => new Sanet.SmartSkating.Dashboard.UWP.App());
 
 			return 0;
 		}
-    }
+		
+		/// <summary>
+		/// Configures global logging
+		/// </summary>
+		/// <param name="factory"></param>
+		static void ConfigureFilters(ILoggerFactory factory)
+		{
+			factory
+				.WithFilter(new FilterLoggerSettings
+					{
+						{ "Uno", LogLevel.Warning },
+						{ "Windows", LogLevel.Warning },
+
+						// Debug JS interop
+						{ "Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug },
+
+						// Generic Xaml events
+						{ "Windows.UI.Xaml", LogLevel.Debug },
+						{ "Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug },
+						{ "Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug },
+						{ "Windows.UI.Xaml.UIElement", LogLevel.Debug },
+
+						// Layout specific messages
+						{ "Windows.UI.Xaml.Controls", LogLevel.Debug },
+						{ "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
+						{ "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
+						{ "Windows.Storage", LogLevel.Debug },
+
+						// Binding related messages
+						{ "Windows.UI.Xaml.Data", LogLevel.Debug },
+
+						// DependencyObject memory references tracking
+						{ "ReferenceHolder", LogLevel.Debug },
+					}
+				)
+#if DEBUG
+				.AddConsole(LogLevel.Debug);
+#else
+				.AddConsole(LogLevel.Information);
+#endif
+		}
+	}
 }
--- Client/Dashboard/SmartSkating.Dashboard/App.xaml.cs
+++ Client/Dashboard/SmartSkating.Dashboard/App.xaml.cs
@@ -1,12 +1,21 @@
-﻿namespace Sanet.SmartSkating.Dashboard
+﻿using Sanet.SmartSkating.Dashboard.Views;
+using Sanet.SmartSkating.ViewModels;
+using Sanet.SmartSkating.Xf.Services;
+using SimpleInjector;
+using Xamarin.Forms;
+
+namespace Sanet.SmartSkating.Dashboard
 {
     public partial class App
     {
-        public App()
+        public App(Container container)
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new LoginView()
+            {
+                ViewModel = new XamarinFormsNavigationService(container).GetViewModel<LoginViewModel>()
+            });
         }
 
         protected override void OnStart()
