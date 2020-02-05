98d5e193457c34555285aa03b57d0c5da87bd32a
Anton Makarevich <anton.makarevich@hotmail.com>
2020-02-05 16:04:07 +0100

    Replace Uno dashboard projects with xf

---
+++ Client/Dashboard/SmartSkating.Dashboard.Wasm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Microsoft.Extensions.Logging;
+
+namespace SmartSkating.Wasm
+{
+	public class Program
+	{
+		static int Main(string[] args)
+		{
+            Windows.UI.Xaml.Application.Start(_ => new SmartSkating.UWP.App());
+
+			return 0;
+		}
+    }
+}
