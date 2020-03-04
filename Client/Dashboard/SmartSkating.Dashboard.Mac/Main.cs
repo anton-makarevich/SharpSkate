cd061db6c3a17625e0229c654d7b9f82789fbfd5
Anton Makarevich <amakarevich@mobiquityinc.com>
2020-03-04 14:47:13 +0100

    Add mac project template for the dashboard

---
+++ Client/Dashboard/SmartSkating.Dashboard.Mac/Main.cs
@@ -0,0 +1,16 @@
+﻿// SmartSkating. Speed Skating activity tracker app
+// Main.cs
+// Copyrigh 2020 amakarevich anton.makarevich@hotmail.com
+using AppKit;
+
+namespace SmartSkating.Dashboard.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ Client/Dashboard/SmartSkating.Dashboard.Mac/ViewController.cs
@@ -0,0 +1,37 @@
+﻿// SmartSkating. Speed Skating activity tracker app
+// ViewController.cs
+// Copyrigh 2020 amakarevich anton.makarevich@hotmail.com
+using System;
+
+using AppKit;
+using Foundation;
+
+namespace SmartSkating.Dashboard.Mac
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
