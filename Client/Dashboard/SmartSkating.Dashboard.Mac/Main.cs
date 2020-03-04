ebb4c031b24667589d842142d33e49e26c8c46cb
Anton Makarevich <amakarevich@mobiquityinc.com>
2020-03-04 16:32:24 +0100

    Remove unused files

--- Client/Dashboard/SmartSkating.Dashboard.Mac/Main.cs
+++ Client/Dashboard/SmartSkating.Dashboard.Mac/Main.cs
@@ -1,15 +1,17 @@
 ﻿// SmartSkating. Speed Skating activity tracker app
 // Main.cs
 // Copyrigh 2020 amakarevich anton.makarevich@hotmail.com
+
 using AppKit;
 
-namespace SmartSkating.Dashboard.Mac
+namespace Sanet.SmartSkating.Dashboard.Mac
 {
     static class MainClass
     {
         static void Main(string[] args)
         {
             NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
             NSApplication.Main(args);
         }
     }
