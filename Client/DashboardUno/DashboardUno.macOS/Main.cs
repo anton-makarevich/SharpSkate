77a78db64587c5ed5105abbbcf2518f274cbfe89
Anton Makarevich <amakarevich@mobiquityinc.com>
2021-02-05 23:56:53 +0100

    Add test uno projects

---
+++ Client/DashboardUno/DashboardUno.macOS/Main.cs
@@ -0,0 +1,14 @@
+using AppKit;
+
+namespace DashboardUno.macOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new App();
+            NSApplication.Main(args);
+        }
+    }
+}
