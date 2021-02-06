b2f0f17212fc6a12fe62b0d961d4c7a3f8ffb500
Anton Makarevich <amakarevich@mobiquityinc.com>
2021-02-06 16:03:16 +0100

    Move page to views folder and rename

--- Client/DashboardUno/DashboardUno.Shared/App.xaml.cs
+++ Client/DashboardUno/DashboardUno.Shared/App.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using DashboardUno.Views;
 
 namespace DashboardUno
 {
@@ -85,7 +86,7 @@ namespace DashboardUno
 					// When the navigation stack isn't restored navigate to the first page,
 					// configuring the new page by passing required information as a navigation
 					// parameter
-					rootFrame.Navigate(typeof(MainPage), e.Arguments);
+					rootFrame.Navigate(typeof(LoginView), e.Arguments);
 				}
 				// Ensure the current window is active
 				window.Activate();
--- Client/DashboardUno/DashboardUno.Shared/Views/LoginView.xaml.cs
+++ Client/DashboardUno/DashboardUno.Shared/Views/LoginView.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace DashboardUno
+namespace DashboardUno.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoginView : Page
     {
-        public MainPage()
+        public LoginView()
         {
             this.InitializeComponent();
         }
