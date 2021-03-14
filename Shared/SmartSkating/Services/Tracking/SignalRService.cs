22fb6fa5360551087c315d6c8b353aacc492da12
sanetby <anton.makarevich@hotmail.com>
2021-03-15 00:12:48 +0100

    Register signalr service

--- Shared/SmartSkating/Services/Tracking/SignalRService.cs
+++ Shared/SmartSkating/Services/Tracking/SignalRService.cs
@@ -10,18 +10,34 @@ namespace Sanet.SmartSkating.Services.Tracking
         private HubConnection? _connection;
         public async Task ConnectToHub(string sessionId)
         {
+            var url = "https://smart-skating-sync.service.signalr.net/client/?hub={sessionId}";
+            var token = "eyJhbGciOiJIUzI1NiIsImtpZCI6IjE0NjQ5MTYwMjkiLCJ0eXAiOiJKV1QifQ.eyJuYmYiOjE2MTU3NjMwMTksImV4cCI6MTYxNTc2NjYxOSwiaWF0IjoxNjE1NzYzMDE5LCJhdWQiOiJodHRwczovL3NtYXJ0LXNrYXRpbmctc3luYy5zZXJ2aWNlLnNpZ25hbHIubmV0L2NsaWVudC8_aHViPWNpbGx1bSBlbGl0IG5vbiJ9.a0lBqgXFIHH8H28rOHfWPOIF5mWSQM451-Vclumg0Ug";
+            // _connection = new HubConnectionBuilder()
+            //     .WithUrl($"{ApiNames.BaseUrl}/{sessionId}", (opts) =>
+            //     {
+            //         opts.Headers.Add("Ocp-Apim-Subscription-Key", ApiNames.AzureApiSubscriptionKey);
+            //     })
+            //     .Build();
             _connection = new HubConnectionBuilder()
-                .WithUrl($"{ApiNames.BaseUrl}/{sessionId}", (opts) =>
+                .WithUrl($"https://smart-skating-sync.service.signalr.net/client/?hub={sessionId}", (opts) =>
                 {
-                    opts.Headers.Add("Ocp-Apim-Subscription-Key", ApiNames.AzureApiSubscriptionKey);
+                    opts.AccessTokenProvider = () => Task.FromResult(token);
                 })
                 .Build();
-            _connection.On<string, string>("ReceiveMessage", (user, message) =>
+            _connection.On<string, string>("newWaypoint", (user, message) =>
             {
                Console.WriteLine($"SIGNALR MSG: {message}");
             });
 
-            await _connection.StartAsync();
+            try
+            {
+                await _connection.StartAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"SIGNALR ERR: {e.Message}");
+            }
+            Console.WriteLine($"SIGNALR STT: {_connection.State}");
         }
 
         public async Task CloseConnection()
