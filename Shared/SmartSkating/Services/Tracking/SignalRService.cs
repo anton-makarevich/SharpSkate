9e740078278af07dc2d238ee075e8713de99ec42
sanetby <anton.makarevich@hotmail.com>
2021-03-14 18:08:12 +0100

    Implement first version of SignalR service

--- Shared/SmartSkating/Services/Tracking/SignalRService.cs
+++ Shared/SmartSkating/Services/Tracking/SignalRService.cs
@@ -1,13 +1,32 @@
-﻿// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.SignalR.Client;
-//
-// namespace Sanet.SmartSkating.Services.Tracking
-// {
-//     public class SignalRService:ISyncService
-//     {
-//         public Task ConnectToHub(string accessToken, string url)
-//         {
-//             //var connection = new HubConnection()
-//         }
-//     }
-// }
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Sanet.SmartSkating.Dto;
+
+namespace Sanet.SmartSkating.Services.Tracking
+{
+    public class SignalRService:ISyncService
+    {
+        private HubConnection? _connection;
+        public async Task ConnectToHub(string sessionId)
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl($"{ApiNames.BaseUrl}/{sessionId}", (opts) =>
+                {
+                    opts.Headers.Add("Ocp-Apim-Subscription-Key", ApiNames.AzureApiSubscriptionKey);
+                })
+                .Build();
+            _connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+               Console.WriteLine($"SIGNALR MSG: {message}");
+            });
+
+            await _connection.StartAsync();
+        }
+
+        public async Task CloseConnection()
+        {
+            if (_connection != null) await _connection.StopAsync();
+        }
+    }
+}
